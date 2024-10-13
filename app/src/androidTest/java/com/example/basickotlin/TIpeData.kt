87e4d7064670593444fe0cb73816e52c58b10750
ImamDwiYanPermana <imampermana244@gmail.com>
2024-10-14 00:18:26 +0700

    membuat fun main dan berisi masing masing tipe data yang beri varibale val dan di setipa string ada tanda $ untuk menyisipkan atau mengunci suatu nama variable yang kita buat

---
+++ app/src/androidTest/java/com/example/basickotlin/TIpeData.kt
@@ -0,0 +1,30 @@
+package com.example.basickotlin
+fun main(){
+    val floatValue: Float = 50.6f
+    println("float: $floatValue")
+
+    val longValue: Long = 7000000L
+    println("long: $longValue")
+
+    val intValue: Int = 10
+    println("int: $intValue")
+
+    val byteValue: Byte = 10
+    println("byte: $byteValue")
+
+    val shortValue: Short = 1000
+    println("SHort: $shortValue")
+
+    val doubleValue: Double = 3.14159
+    println("Double: $doubleValue")
+
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+    val stringValue: String = "Hello world"
+    println("String: $stringValue")
+
+}
